8d6ee25137ce3f612793b405b0a864b37a859ebf
Giovanni Bechis <giovanni.behis@gmail.com>
2018-03-28 19:32:28 +0200

    paginated fetch

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 const DEFAULT_QUERY = 'php';
+const DEFAULT_HPP = '10';
+
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 // ES5
 /* function isSearched(searchTerm) {
@@ -36,11 +40,23 @@ class App extends Component {
   }
 
   setSearchTopStories(result) {
-    this.setState({ result });
+    //this.setState({ result });
+    const { hits, page } = result;
+    const oldHits = page !== 0
+      ? this.state.result.hits
+      : [];
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+    this.setState({
+      result: { hits: updatedHits, page }
+    });
+
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(e => e);
@@ -73,6 +89,7 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state;
+    const page = (result && result.page) || 0;
     
     return (     
       <div className="page" >
@@ -85,13 +102,17 @@ class App extends Component {
           Search
         </Search>
         </div>
-        {result
-          ? <Table
+        {result &&
+          <Table
             list={result.hits}
             onDismiss={this.onDismiss}
           />
-          : null
         }
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+            More
+          </Button>
+        </div>
       </div >
     );
   }
