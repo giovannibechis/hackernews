552f2f93c4ece915f25696aa6d77dd062ca6daf2
Giovanni Bechis <giovanni.behis@gmail.com>
2018-05-10 21:53:54 +0200

    ref atrributes

--- src/App.js
+++ src/App.js
@@ -184,30 +184,61 @@ class App extends Component {
 }*/
   
 
-const Search = ({
-    value,
-    onChange,
-    onSubmit,
-    children
-  }) =>
-  <form onSubmit={onSubmit}>
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-    />
-    <button type="submit">
-      {children}
-    </button>
-  </form>
+// const Search = ({
+//     value,
+//     onChange,
+//     onSubmit,
+//     children
+//   }) =>
+//   <form onSubmit={onSubmit}>
+//     <input
+//       type="text"
+//       value={value}
+//       onChange={onChange}
+//     />
+//     <button type="submit">
+//       {children}
+//     </button>
+//   </form>
 
-Search.propTypes = {
-  value:PropTypes.string,
-  onChange: PropTypes.func.isRequired,
-  onSubmit: PropTypes.func.isRequired,
-  children: PropTypes.node.isRequired
-};
+// Search.propTypes = {
+//   value:PropTypes.string,
+//   onChange: PropTypes.func.isRequired,
+//   onSubmit: PropTypes.func.isRequired,
+//   children: PropTypes.node.isRequired
+// };
 
+class Search extends Component {
+
+  componentDidMount() {
+    if (this.input) {
+      this.input.focus();
+    }
+  }
+    
+  render() {
+    const {
+      value,
+      onChange,
+      onSubmit,
+      children
+    } = this.props;
+    return (
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          ref={(node) => { this.input = node; }}
+        />
+        <button type="submit">
+          {children}
+        </button>
+      </form>
+    );
+  }
+}
+  
 
 const Table =  ({list, onDismiss}) =>
     <div className="table">
